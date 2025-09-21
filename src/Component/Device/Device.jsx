7310b272646b5ff4e26c38b83d0ca6dbd343291d
mohammademon393 <mdemon357ep@gmail.com>
2025-09-21 17:19:47 +0600

    show device data in display

--- src/Component/Device/Device.jsx
+++ src/Component/Device/Device.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
 
-const Device = () => {
-    return (
-        <div>
-            <h3>device</h3>
-        </div>
-    );
+
+const Device = ({ device }) => {
+    console.log(device);
+    
+  return (
+    <div>
+      <img src={device.image} alt="" />
+      <h3>{device.model}</h3>
+    </div>
+  );
 };
 
 export default Device;
--- src/Component/Devices/Devices.jsx
+++ src/Component/Devices/Devices.jsx
@@ -1,16 +1,18 @@
 import { use } from "react";
+import Device from "../Device/Device";
 
 
 const Devices = ({ devicePromise }) => {
 
-    const devices = use(devicePromise);
-    console.log(devices);
-    
+    const devices = use(devicePromise);  
 
 
   return (
     <div>
-      <h3>deviessss data file</h3>
+      <h3>Total devies: {devices.length}</h3>
+      {devices.map((device) => (
+        <Device key={device.id} device={device}></Device>
+      ))}
     </div>
   );
 };
