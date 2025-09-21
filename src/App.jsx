5a95cc4e7e27ef825516d2324372948aba097dae
mohammademon393 <mdemon357ep@gmail.com>
2025-09-21 16:21:20 +0600

    Json data create and load this data

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import './App.css'
 
 function App() {
 
+  // data load 
+  const devicePromise = fetch('./device.json').then(res => res.json()).then(data => console.log(data)
+  )
+  console.log(devicePromise);
+  
+
   return (
     <>
       <h1>React awesome device</h1>
